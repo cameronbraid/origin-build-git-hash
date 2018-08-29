3ffcc93e49886a0133002ea171327f17efcbd4f8
Cameron Braid <cameron@drivenow.com.au>
2018-08-29 11:15:26 +1000

    wip

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-process.on("SIGTERM", () => {
-    console.log("SIGTERM")
-    process.exit(0)
-})
+
+async function main() {
+
+    process.on("SIGTERM", () => {
+        console.log("SIGTERM")
+        process.exit(0)
+    })
+
+    console.log(process.pid)
+
+    while(true) await new Promise(resolve=>setTimeout(resolve, 10))
+}
+
+main()
